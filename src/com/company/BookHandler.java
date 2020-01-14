1a6ecb238664bc7b6e6f71d0a438d73d99dfc18f
Johan <johanomwikstrom@gmail.com>
2020-01-14 14:39:35 +0100

    Created HP-Trilogy and LotR-Trilogy in BookHandler-Class

--- src/com/company/BookHandler.java
+++ src/com/company/BookHandler.java
@@ -1,4 +1,37 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class BookHandler {
+
+    ArrayList<Book> listOfAllBooks = new ArrayList<>();
+
+    public BookHandler(){
+        createNewBook();
+    }
+
+
+    // Create new object in form of a book, add it to "listOfAllBooks"
+    public void createNewBook(){
+
+        // Harry Potter Trilogy
+        listOfAllBooks.add(new Book("Harry Potter and the Philosopher's Stone", "J.K Rowling",
+                "Harry Potter, a young boy realizes he is not an ordinary boy, but a wizard"));
+        listOfAllBooks.add(new Book("Harry Potter and the Chamber of Secrets", "J.K Rowling",
+                "Harry Potter countinues his adventures in Hogwarts, but another enemy lurks in the"
+                        + " " + "chamber underneeth the school."));
+        listOfAllBooks.add(new Book("Harry Potter and the Prisoner of Azkaban", "J.K Rowling",
+                "Harry Potter encounters his worst enemy so far, what or who is this mysterious creature."));
+
+        // Lord of the Rings Trilogy
+        listOfAllBooks.add(new Book("Lord of the Rings: Felloship of the Ring", "J.R.R. Tolkien",
+                "Frodo, a young hobbit living in peace, encounters a powerful object and" + " " +
+                        "got sent out on a great adventure"));
+        listOfAllBooks.add(new Book("Lord of the Rings: The two towers", "J.R.R. Tolkien",
+                "Fordo and Sam continue their way towards Mordor, but get in the middle of the great war"));
+        listOfAllBooks.add(new Book("Lord of the Rings: The Return of the King", "J.R.R. Tolkien",
+                "As Smeagol leads the way, Frodo, Sam and the others stands before their greatest challenge yet"));
+
+    }
+
 }
