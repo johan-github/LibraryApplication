37faf15682158964185fd1254bec45e73d729a2b
Johan <johanomwikstrom@gmail.com>
2020-01-16 15:08:55 +0100

    Method in BookHandler, possible search author by entering title

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -6,13 +6,14 @@ public class Program {
     public Program() {
 
         BookHandler bookHandler = new BookHandler();
-        bookHandler.showBookAuthor();
-        System.out.println();
-        bookHandler.showBookTitle();
-        System.out.println();
-        bookHandler.showBookDescription();
-        System.out.println();
-        bookHandler.showAllBooks();
+        // bookHandler.showBookAuthor();
+        // System.out.println();
+        // bookHandler.showBookTitle();
+        // System.out.println();
+        // bookHandler.showBookDescription();
+        // System.out.println();
+        // bookHandler.showAllBooks(); */
+        //bookHandler.showAuthorCertainBook("Lord of the Rings: Fellowship of the Ring");
 
     }
 
